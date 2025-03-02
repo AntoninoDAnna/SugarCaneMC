84114e294598b505b791501193eb222080f1bab9
Antonino D'Anna <anda982008@gmail.com>
2025-03-02 17:49:40 +0100

    added MC_length

--- parameters.jl
+++ parameters.jl
@@ -2,7 +2,7 @@ const seed = 1234
 const Ns = 100; #number of sugarcanes
 const No = 10; #numbers of observers
 const mode = 2; #how to organize observers: 1 = randomly, 2 =optimized
-
+const MC_length = 10000
 ## sugar cane constants
 const prob::Float64 = 3.0/4096.0 # assuming RandomTickSpeed is 3
 const max_random_tick::Int64 = 16;
